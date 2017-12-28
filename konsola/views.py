537b4a32f99cc4b84d253914570057a54af15a79
Mariusz Kostrzewa <Mariusz.Kostrzewa@orange.com>
2017-12-28 17:47:29 +0100

    commit

--- konsola/views.py
+++ konsola/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import Pomiar
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    latest_results=Pomiar.objects.order_by('-pub_date')[:5]
+    template=loader.get_template('konsola/index.html')
+    context={
+        'latest_results':latest_results,
+        }
+    return HttpResponse(template.render(context, request))
